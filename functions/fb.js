284aead99d1adbfd9b5dfe46067ca85f7c42f5e9
Erick Jansen <janseniter@gmail.com>
2020-08-16 17:11:26 +0200

    fix typo

--- functions/fb.js
+++ functions/fb.js
@@ -2,24 +2,21 @@ const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.stringify({
-        type: 'service_account',
-        project_id: 'rickysquest-4dae6',
-        private_key_id: process.env.FB_KEY_ID,
-        private_key:
-          process.env.FB_KEY[0] === '-'
-            ? process.env.FB_KEY
-            : JSON.parse(process.env.FB_KEY),
-        client_email: process.env.FB_CLIENT_EMAIL,
-        client_id: process.env.CLIENT_ID,
-        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-        token_uri: 'https://oauth2.googleapis.com/token',
-        auth_provider_x509_cert_url:
-          'https://www.googleapis.com/oauth2/v1/certs',
-        client_x509_cert_url: process.env.CLIENT_CERT_URL,
-      })
-    ),
+    credential: admin.credential.cert({
+      type: 'service_account',
+      project_id: 'rickysquest-4dae6',
+      private_key_id: process.env.FB_KEY_ID,
+      private_key:
+        process.env.FB_KEY[0] === '-'
+          ? process.env.FB_KEY
+          : JSON.parse(process.env.FB_KEY),
+      client_email: process.env.FB_CLIENT_EMAIL,
+      client_id: process.env.FB_CLIENT_ID,
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url: process.env.CLIENT_CERT_URL,
+    }),
   });
 }
 
