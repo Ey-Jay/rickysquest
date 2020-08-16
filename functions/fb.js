008519b8befebbbd9b6a6710a6aae758f89e4cc1
Erick Jansen <janseniter@gmail.com>
2020-08-16 16:59:09 +0200

    more iissuueess

--- functions/fb.js
+++ functions/fb.js
@@ -2,16 +2,16 @@ const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    type: 'service_account',
     credential: admin.credential.cert({
-      clientEmail: process.env.FB_CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
+      type: 'service_account',
+      projectId: 'rickysquest-4dae6',
+      private_key_id: process.env.FB_KEY_ID,
       privateKey:
         process.env.FB_KEY[0] === '-'
           ? process.env.FB_KEY
           : JSON.parse(process.env.FB_KEY),
-      private_key_id: process.env.FB_KEY_ID,
-      projectId: 'rickysquest-4dae6',
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_uri: 'https://oauth2.googleapis.com/token',
       auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
