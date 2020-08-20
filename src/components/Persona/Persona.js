43cbb96841e92cbe4c45ec663aba28ee4a279e16
Joss Doebler <mail@joss-doebler.de>
2020-08-20 17:20:21 +0200

    Added GraphQL frontend part

--- src/components/Persona/Persona.js
+++ src/components/Persona/Persona.js
@@ -1,15 +1,32 @@
 import React, { useContext, useEffect } from 'react';
 import { AvatarContainer, Collection, Follower } from './styled';
 import { ReactComponent as AvatarSVG } from 'assets/avatar.svg';
-import { GameContext } from 'context/game';
+import { useQuery, gql } from '@apollo/client';
+// import { GameContext } from 'context/game';
+
+const GET_CHARACTERS = (ids) => gql`
+  query Characters {
+    characters(id: [${[...ids]}]) {
+      id
+      name
+      image
+    }
+  }
+`;
 
 const Persona = () => {
-  const { characters, setFollowersWithDB } = useContext(GameContext);
+  const { loading, data, error } = useQuery(
+    GET_CHARACTERS([7, 8, 345, 234, 123])
+  );
+  // const { characters, setFollowersWithDB } = useContext(GameContext);
 
-  useEffect(() => {
-    setFollowersWithDB();
-    // eslint-disable-next-line
-  }, []);
+  // useEffect(() => {
+  //   setFollowersWithDB();
+  //   // eslint-disable-next-line
+  // }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <>
@@ -19,7 +36,7 @@ const Persona = () => {
 
       <h2>Follower Collection</h2>
       <Collection>
-        {characters.map((character) => (
+        {data.characters.map((character) => (
           <Follower key={character.id}>
             <img src={character.image} alt={character.name} />
             <p>{character.name}</p>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from 'context/AuthProvider';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyles } from 'theme';
 import { GameContextProvider } from 'context/game';
 
+const client = new ApolloClient({
+  uri: '/.netlify/functions/graphql',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <GameContextProvider>
-          <Router>
-            <GlobalStyles />
-            <App />
-          </Router>
-        </GameContextProvider>
-      </ThemeProvider>
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <GameContextProvider>
+            <Router>
+              <GlobalStyles />
+              <App />
+            </Router>
+          </GameContextProvider>
+        </ThemeProvider>
+      </AuthProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
