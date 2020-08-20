958019be8af8543dcb511b0695cdefdb3c1200a4
Joss Doebler <mail@joss-doebler.de>
2020-08-20 11:28:20 +0200

    Add getQuizData function

---
+++ src/base/getQuizData.js
@@ -0,0 +1,16 @@
+import { db } from './index';
+
+async function getQuizData(id) {
+  return await db
+    .collection('quizzes')
+    .doc(id.toString())
+    .get()
+    .then((doc) => {
+      return doc.data();
+    })
+    .catch((err) => {
+      return console.error(err);
+    });
+}
+
+export default getQuizData;
