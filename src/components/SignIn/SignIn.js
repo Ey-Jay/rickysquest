82096d0c1872ae881ebe5e04f16f692f9aa14b84
Joss Doebler <mail@joss-doebler.de>
2020-08-23 16:05:52 +0200

    Google branding on sign in button & logo adjustments

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,7 +2,14 @@ import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import * as firebase from 'firebase/app';
 
-import { Container, Logo } from './styled';
+import {
+  Container,
+  Logo,
+  GoogleButton,
+  GoogleIconWrapper,
+  GoogleIcon,
+  ButtonText,
+} from './styled';
 import { ReactComponent as LogoSVG } from 'assets/rick.svg';
 import app from '../../base';
 import { AuthContext } from 'context/AuthContext';
@@ -38,7 +45,17 @@ const SignIn = () => {
         <LogoSVG />
       </Logo>
       <Container>
-        <button onClick={signIn}>Login/Signup</button>
+        <GoogleButton onClick={signIn}>
+          <GoogleIconWrapper>
+            <GoogleIcon
+              class="google-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            />
+          </GoogleIconWrapper>
+          <ButtonText>
+            <span>Sign in with Google</span>
+          </ButtonText>
+        </GoogleButton>
       </Container>
     </>
   );
